8ed213086339714e68d687c90ef5d0ee002a0a31
Joe Kutner <jpkutner@gmail.com>
2015-08-05 16:10:07 -0500

    Fixed seg fault by flushing stdout and stderr

--- src/java_support.c
+++ src/java_support.c
@@ -43,7 +43,7 @@ mrb_java_support_exec(mrb_state *mrb, mrb_value obj)
 
   for (int i = 0; i < jvmOptCount; i++) {
     jvmOptions[i].extraInfo = 0;
-    jvmOptions[i].optionString = (char*) mrb_string_value_cstr(mrb, &argv[i+1]);
+    jvmOptions[i].optionString = mrb_string_value_cstr(mrb, &argv[i+1]);
     if (strcmp("-client", jvmOptions[i].optionString) == 0) {
       mrb_raise(mrb, E_ARGUMENT_ERROR, "-client is not a valid option");
     } else if (strcmp("-server", jvmOptions[i].optionString) == 0) {
@@ -51,55 +51,22 @@ mrb_java_support_exec(mrb_state *mrb, mrb_value obj)
     }
   }
 
-  // jvmOptions = new JavaVMOption[options.size()];
-  // int i = 0;
-  // for (list<string>::iterator it = options.begin(); it != options.end(); ++it, ++i) {
-  //     string &option = *it;
-  //     logMsg("\t%s", option.c_str());
-  //     jvmOptions[i].optionString = (char *) option.c_str();
-  //     jvmOptions[i].extraInfo = 0;
-  // }
-  // JavaVMOption options[0];
-  // options[2].optionString = "-Djava.library.path=/";  /* set native library path */
-  // options[0].optionString = "Main";
-
+  // JavaVMOption jvmOptions[0];
   jvmArgs.options = jvmOptions;
-  jvmArgs.nOptions = jvmOptCount;
+  jvmArgs.nOptions = 0;
   jvmArgs.version = JNI_VERSION_1_4;
   jvmArgs.ignoreUnrecognized = JNI_FALSE;
 
   CreateJavaVM_t* createJavaVM = NULL;
 
-#if defined(_WIN32) || defined(_WIN64)
-  // PrepareDllPath prepare(jvmLauncher->javaBinPath.c_str());
-  // hDll = LoadLibrary(jvmLauncher->javaDllPath.c_str());
-  // if (!hDll) {
-  //   // log "Cannot load %s.", javaDllPath.c_str());
-  //   return mrb_false_value();
-  // }
-  //
-  // CreateJavaVM createJavaVM = (CreateJavaVM) GetProcAddress(hDll, JNI_CREATEVM_FUNC);
-  // if (!createJavaVM) {
-  //     //logErr(true, true, "GetProcAddress for %s failed.", JNI_CREATEVM_FUNC);
-  //     return false;
-  // }
-  //
-  // if (createJavaVM(&jvm, &env, &jvmArgs) < 0) {
-  //   return mrb_false_value();
-  // }
-  // return mrb_true_value();
-#else
-
   // jli needs to be loaded on OSX because otherwise the OS tries to run the system Java
-  void *libjli = dlopen(strcat(java_home, "/jre/lib/jli/libjli.dylib"), RTLD_NOW + RTLD_GLOBAL);
-  void *libjvm = dlopen(strcat(java_home, "/jre/lib/server/libjvm.dylib"), RTLD_NOW + RTLD_GLOBAL);
-
+  void *libjli = dlopen("/Library/Java/JavaVirtualMachines/jdk1.8.0_51.jdk/Contents/Home/jre/lib/jli/libjli.dylib", RTLD_NOW + RTLD_GLOBAL);
+  void *libjvm = dlopen("/Library/Java/JavaVirtualMachines/jdk1.8.0_51.jdk/Contents/Home/jre/lib/server/libjvm.dylib", RTLD_NOW + RTLD_GLOBAL);
 
   createJavaVM = (CreateJavaVM_t*) dlsym(libjvm, "JNI_CreateJavaVM");
   if (createJavaVM == NULL) {
     mrb_raise(mrb, E_RUNTIME_ERROR, "Could not load JVM");
   }
-#endif
 
   if (createJavaVM(&jvm, (void**)&env, &jvmArgs) < 0) {
     mrb_raise(mrb, E_RUNTIME_ERROR, "JVM creation failed");
@@ -121,37 +88,16 @@ mrb_java_support_exec(mrb_state *mrb, mrb_value obj)
   jstring jstringArg = (*env)->NewStringUTF(env, "");
 
   jobjectArray mainArgs = (*env)->NewObjectArray(env, 0, jclassString, jstringArg);
-  // if (!mainArgs) {
-  //     logErr(false, true, "NewObjectArray() failed");
-  //     return false;
-  // }
-
-  // int i = 0;
-  // for (list<string>::iterator it = args.begin(); it != args.end(); ++it, ++i) {
-  //     string &arg = *it;
-  //     jstring jstringArg = (*env)->NewStringUTF(arg.c_str());
-  //     if (!jstringArg) {
-  //         logErr(false, true, "NewStringUTF() failed");
-  //         return false;
-  //     }
-  //     (*env)->SetObjectArrayElement(mainArgs, i, jstringArg);
-  // }
 
   (*env)->CallStaticVoidMethod(env, mainClass, mainMethod, mainArgs);
 
   if (env && (*env)->ExceptionOccurred(env)) {
     (*env)->ExceptionDescribe(env);
   }
+  (*jvm)->DestroyJavaVM(jvm);
 
-  if (jvm) {
-    (*jvm)->DestroyJavaVM(jvm);
-  }
-
-#if defined(_WIN32) || defined(_WIN64)
-  if (hDll) {
-    FreeLibrary(hDll);
-  }
-#endif
+  fflush(stdout);
+  fflush(stderr);
 
   return mrb_true_value();
 }
