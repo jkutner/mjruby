25f1189048ed7f2175ee3e38e9bcc5a2a523d976
Joe Kutner <jpkutner@gmail.com>
2015-08-20 08:06:04 -0500

    Cleaned up includes

--- src/java_support.c
+++ src/java_support.c
@@ -15,18 +15,7 @@
   #include <dlfcn.h>
   #include <stdlib.h>
   #include <unistd.h>
-
-  // #include <sys/types.h>
-  // #include <sys/time.h>
-  // #include <sys/wait.h>
-  // #include <sys/select.h>
   #include <signal.h>
-  //
-  // #include <errno.h>
-  // #include <stdio.h>
-  // #include <stdlib.h>
-  // #include <string.h>
-  // #include <time.h>
 #endif
 
 #include <jni.h>
