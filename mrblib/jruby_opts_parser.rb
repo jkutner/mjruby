19d7b07d738c47d6b3a5933bd6fec349a238a44e
Joe Kutner <jpkutner@gmail.com>
2015-08-23 08:58:32 -0500

    Simplified the combining of separate opts

--- mrblib/jruby_opts_parser.rb
+++ mrblib/jruby_opts_parser.rb
@@ -89,12 +89,8 @@ class JRubyOptsParser
         end
       elsif ['-C', '-e', '-I', '-S'].include?(opt[0..1])
         # Match switches that take an argument
-        if opt.size == 2
-          combined_opt = "#{opt}#{opts.shift}"
-          @ruby_opts << combined_opt
-        else
-          @ruby_opts << opt
-        end
+        opt += opts.shift if opt.size == 2
+        @ruby_opts << opt
       elsif opt == "--manage"
         @java_opts << "-Dcom.sun.management.jmxremote"
         @java_opts << "-Djruby.management.enabled=true"
