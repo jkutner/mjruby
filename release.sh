06359f34f7bc4b8fa72bfb26a84193b0cbd40458
Joe Kutner <jpkutner@gmail.com>
2015-09-07 08:23:05 -0500

    Changed to target dir

--- release.sh
+++ release.sh
@@ -1,33 +1,39 @@
-mkdir -p releases
+set -e
+
+docker-compose run clean
+
+docker-compose run compile
+
+mkdir -p target
 
 cd mruby/build/
 
 cd i386-apple-darwin14/bin
 zip mjruby_macosx_bin.zip mjruby
-mv *.zip ../../../../releases
+mv *.zip ../../../../target
 cd ../..
 
 cd x86_64-apple-darwin14/bin
 zip mjruby_macosx_x86_64_bin.zip mjruby
-mv *.zip ../../../../releases
+mv *.zip ../../../../target
 cd ../..
 
 cd i686-pc-linux-gnu/bin
 zip mjruby_linux_bin.zip mjruby
-mv *.zip ../../../../releases
+mv *.zip ../../../../target
 cd ../..
 
 cd x86_64-pc-linux-gnu/bin
 zip mjruby_linux_x86_64_bin.zip mjruby
-mv *.zip ../../../../releases
+mv *.zip ../../../../target
 cd ../..
 
 cd i686-w64-mingw32/bin
 zip mjruby_win_exe.zip mjruby.exe
-mv *.zip ../../../../releases
+mv *.zip ../../../../target
 cd ../..
 
 cd x86_64-w64-mingw32/bin
 zip mjruby_win_x86_64_exe.zip mjruby.exe
-mv *.zip ../../../../releases
+mv *.zip ../../../../target
 cd ../..
